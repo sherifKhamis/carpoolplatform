98d52507ff2c67ac50199b387900c18c304de54b
sherif.khamis <sherifookhamis@gmail.com>
2025-03-23 19:17:54 +0100

    Initialized backend again

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/sherifkhamis/PycharmProjects/carpoolplatform/venv"
+set -gx VIRTUAL_ENV "/Users/sherifkhamis/Projects/carpoolplatform/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
