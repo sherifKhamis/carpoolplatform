6fc9559eda986b748f5c1d1c0bfdca2fccf21ff5
sherif.khamis <sherifookhamis@gmail.com>
2025-03-23 10:55:25 +0100

    Configure .gitignore file for Django and Vue project

--- backend/carpool/admin.py
+++ backend/carpool/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Car, Trip, Reservation
 
-# Register your models here.
+admin.site.register(Car)
+admin.site.register(Trip)
+admin.site.register(Reservation)
